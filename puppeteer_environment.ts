f58aebe9e43a9bf8cccc5b5d1dd9a0c9895e3c1e
jj4th <john.andrichak@gmail.com>
2018-01-24 18:48:46 -0600

    chore(test): Add statistical analysis.

--- puppeteer_environment.ts
+++ puppeteer_environment.ts
@@ -3,8 +3,30 @@ import * as puppeteer from 'puppeteer';
 import * as fs from 'fs-extra';
 import * as os from 'os';
 import * as path from 'path';
+import * as vm from 'vm';
 
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
+
+const METRICS_FILE = '.metrics.json';
+
+const loadMetricsData = () => {
+  if (fs.existsSync(METRICS_FILE)) {
+    try {
+      return fs.readJsonSync(METRICS_FILE) as MetricsData;
+    } catch (e) {
+      // ignore errors.
+    }
+  }
+  return {};
+};
+
+const saveMetricsData = (data: MetricsData) => {
+  try {
+    fs.writeFileSync(METRICS_FILE, JSON.stringify(data, null, 2));
+  } catch (e) {
+    console.error('Failed to write performance file.');
+  }
+}
 
 class PuppeteerEnvironment extends NodeEnvironment {
   async setup() {
@@ -14,9 +36,15 @@ class PuppeteerEnvironment extends NodeEnvironment {
     if (!wsEndpoint) {
       throw new Error('wsEndpoint not found');
     }
+    this.global.__METRICS__ = loadMetricsData();
     this.global.__BROWSER__ = await puppeteer.connect({
       browserWSEndpoint: wsEndpoint,
     })
+  }
+
+  async teardown() {
+    saveMetricsData(this.global.__METRICS__);
+    await super.teardown();
   }
 }
 
--- types/jest-environment-node.d.ts
+++ types/jest-environment-node.d.ts
@@ -1,9 +1,11 @@
 // Shell typedef to satisfy TypeScript
 declare module 'jest-environment-node' {
+    import * as vm from 'vm';
     class NodeEnvironment {
         global: NodeJS.Global;
         constructor(config: {});
         setup(): Promise<void>;
+        teardown(): Promise<void>;
     }
     export = NodeEnvironment;
 }
